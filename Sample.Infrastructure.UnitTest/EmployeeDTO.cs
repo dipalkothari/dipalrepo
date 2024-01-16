c794905df12210d559107f6e043304d2721c8b98
DipalKothari <dipal.kothari1@t-mobile.com>
2024-01-17 01:32:57 +0530

    Added DTO class with automapper

--- Sample.Infrastructure.UnitTest/EmployeeDTO.cs
+++ Sample.Infrastructure.UnitTest/EmployeeDTO.cs
@@ -6,7 +6,7 @@ namespace Sample.Infrastructure.UnitTest
 {
     public class EmployeeDTO
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
--- Sample.Services/EmployeeService.cs
+++ Sample.Services/EmployeeService.cs
@@ -1,8 +1,114 @@
-﻿using System;
+﻿using AutoMapper;
+using Sample.Domains;
+using Sample.Infrastructure.UnitTest;
+using Sample.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Sample.Services
 {
     public class EmployeeService
     {
+        public readonly EmployeeRepository _repository;
+        private readonly IMapper _mapper;
+
+        public EmployeeService(EmployeeRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<EmployeeDTO> Save(EmployeeDTO employeeDto)
+        {
+            try
+            {
+                var result = _repository.GetEmployeeById(employeeDto.Id);
+                if (result == null)
+                {
+                    var addedUser = await _repository.AddEmployee(_mapper.Map<Employee>(employeeDto));
+
+                    return _mapper.Map<EmployeeDTO>(addedUser);
+                }
+                else
+                {
+                    var updateUser = await _repository.UpdateEmployee(_mapper.Map<Employee>(employeeDto));
+
+                    return _mapper.Map<EmployeeDTO>(updateUser);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        public async Task<List<EmployeeDTO>> GetAll(string empValue)
+        {
+            try
+            {
+                if (empValue == null)
+                {
+                    throw new ArgumentNullException(nameof(empValue));
+                }
+                else
+                {
+                    var result = await _repository.GetAllEmployee(empValue);
+                    List<EmployeeDTO> employeeDTOs = new List<EmployeeDTO>();
+                    foreach (var em in result)
+                    {
+                        EmployeeDTO dTO = new EmployeeDTO();
+                        dTO.FullName = string.Concat(em.FirstName, " ", em.LastName);
+                        dTO.FirstName = em.FirstName;
+                        dTO.LastName = em.LastName;
+                        dTO.Age = em.Age;
+                        dTO.Gender = em.Gender;
+                        employeeDTOs.Add(dTO);
+                    }
+                    return _mapper.Map<List<EmployeeDTO>>(employeeDTOs);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<EmployeeDTO> GetEmployeeById(string Id)
+        {
+            try
+            {
+                var employee = await _repository.GetEmployeeById(Id);
+                EmployeeDTO employeeDTO = new EmployeeDTO();
+                employeeDTO.FullName = employee.FirstName + employee.LastName;
+                employeeDTO.FirstName = employee.FirstName;
+                employeeDTO.LastName = employee.LastName;
+                employeeDTO.Age = employee.Age;
+                employeeDTO.Gender = employee.Gender;
+                return employeeDTO;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        public void DeleteEmployee(Guid Id)
+        {
+            try
+            {
+                if (Id != null)
+                {
+                    _repository.DeleteEmployee(Id);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
