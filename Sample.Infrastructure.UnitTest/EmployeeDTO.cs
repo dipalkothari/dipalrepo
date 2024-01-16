b8d1939a97fa4e45780a33943707e102c82e7cbe
DipalKothari <dipal.kothari1@t-mobile.com>
2024-01-17 01:21:02 +0530

    Added Automapper classes

---
+++ Sample.Infrastructure.UnitTest/EmployeeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.Infrastructure.UnitTest
+{
+    public class EmployeeDTO
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public char Gender { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
