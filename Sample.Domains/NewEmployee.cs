da9db00d8be8afdf42c6a279e87b10b4c24939f9
DipalKothari <dipal.kothari1@t-mobile.com>
2024-01-17 00:09:53 +0530

    Added employee property

---
+++ Sample.Domains/NewEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sample.Domains
+{
+
+    public class NewEmployee
+    {
+        [Key]
+        [Required(ErrorMessage = "Id is required")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "FirstName is required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "LastName is required")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Age is required")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        public char Gender { get; set; }
+
+    }
+}
